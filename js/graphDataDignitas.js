1dd99055c38f9a435472e205970ca5fe67990c07
samparkes <46539458+samparkes@users.noreply.github.com>
2020-03-12 00:40:28 +0000

    Dignitas Player Graphs

---
+++ js/graphDataDignitas.js
@@ -0,0 +1,55 @@
+
+
+  function updateChart()/*Vergae Gold Per Minute*/
+  {
+    statChart.data.datasets[0].data = [403, 318, 395, 398, 253];
+    statChart.data.datasets[0].backgroundColor = [
+	pattern.draw('square', '#ff6384'),
+	pattern.draw('circle', '#36a2eb'),
+	pattern.draw('diamond', '#cc65fe'),
+	pattern.draw('triangle', '#ffce56'),
+	pattern.draw('circle', '#ff6384')],
+    statChart.options.title.text = 'Average Gold Per Minute';
+	statChart.options.scales.yAxes[0].ticks.max = 500;
+    statChart.update();
+  }
+
+  function updateChart2()/*Vergae CS Per Minute*/
+  {
+    statChart.data.datasets[0].data = [8.2, 5, 8.8, 8.9, 0.9];
+    statChart.data.datasets[0].backgroundColor = [
+    pattern.draw('square', '#8DBF8B'),
+    pattern.draw('circle', '#F09856'),
+    pattern.draw('diamond', '#FADC9C'),
+    pattern.draw('triangle', '#ffce56'),
+	pattern.draw('square', '#8DBF8B')],
+	statChart.options.title.text = 'CS Per Minute';
+    statChart.options.scales.yAxes[0].ticks.max = 10;
+    statChart.update();
+  }
+
+  function updateChart3()//KDA
+  {
+    statChart.data.datasets[0].data = [2.9, 3.2, 4, 2.7, 3.2];
+    statChart.data.datasets[0].backgroundColor = [
+    pattern.draw('square', '#EFA8EA'),
+    pattern.draw('circle', '#18A4E0'),
+    pattern.draw('diamond', '#FADC9C'),
+    pattern.draw('triangle', '#97E5EF')],
+    statChart.options.title.text = 'Player 3';
+    	statChart.options.scales.yAxes[0].ticks.max = 10;
+    statChart.update();
+  }
+
+  function updateChart4()//Win %
+  {
+    statChart.data.datasets[0].data = [62, 52, 45, 27, 53];
+    statChart.data.datasets[0].backgroundColor = [
+    pattern.draw('square', '#AAD0D9'),
+    pattern.draw('circle', '#EED5D1'),
+    pattern.draw('diamond', '#A7BF2E'),
+    pattern.draw('triangle', '#F95A37')],
+    statChart.options.title.text = 'Player 4';
+    	statChart.options.scales.yAxes[0].ticks.max = 100;
+    statChart.update();
+  }
