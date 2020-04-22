3994a5b8aaeec7f4a17526b75498a54c21102d55
HuxleyK <44905925+HuxleyK@users.noreply.github.com>
2020-04-22 19:55:28 +0100

    Late changes

--- js/update.js
+++ js/update.js
@@ -1,63 +1,63 @@
 function updateBracket()
 {
-	
+
 	var OneOne = document.getElementById("OneOne").value;
 	var OneTwo = document.getElementById("OneTwo").value;
 	var OneOneScore = algo(OneOne,OneTwo);
 	var OneTwoScore = 5 - OneOneScore;
-	
+
 	var TwoOne = document.getElementById("TwoOne").value;
 	var TwoTwo = document.getElementById("TwoTwo").value;
 	var TwoOneScore = algo(TwoOne,TwoTwo);
 	var TwoTwoScore = 5 - TwoOneScore;
-	
+
 	var ThreeOne = document.getElementById("ThreeOne").value;
 	var ThreeTwo = document.getElementById("ThreeTwo").value;
 	var ThreeOneScore = algo(ThreeOne,ThreeTwo);
 	var ThreeTwoScore = 5 - ThreeOneScore;
-	
+
 	var FourOne = document.getElementById("FourOne").value;
 	var FourTwo = document.getElementById("FourTwo").value;
 	var FourOneScore = algo(FourOne,FourTwo);
 	var FourTwoScore = 5 - FourOneScore;
-	
-	
+
+
 	if (OneOneScore > OneTwoScore) {
 		var QuarterOneWinner = OneOne;
 	}
 	else {
 		var QuarterOneWinner = OneTwo;
 	}
-	
+
 	if (TwoOneScore > TwoTwoScore) {
 		var QuarterTwoWinner = TwoOne;
 	}
 	else {
 		var QuarterTwoWinner = TwoTwo;
 	}
-	
+
 	var SemiOneOneScore = algo(QuarterOneWinner, QuarterTwoWinner);
 	var SemiOneTwoScore = 5 - SemiOneOneScore;
-	
-	
+
+
 	if (ThreeOneScore > ThreeTwoScore) {
 		var QuarterThreeWinner = ThreeOne;
 	}
 	else {
 		var QuarterThreeWinner = ThreeTwo;
 	}
-	
+
 	if (FourOneScore > FourTwoScore) {
 		var QuarterFourWinner = FourOne;
 	}
 	else {
 		var QuarterFourWinner = FourTwo;
 	}
-	
+
 	var SemiTwoOneScore = algo(QuarterThreeWinner, QuarterFourWinner);
 	var SemiTwoTwoScore = 5 - SemiTwoOneScore;
-	
-	
+
+
 	if (SemiOneOneScore > SemiOneTwoScore) {
 		var SemiOneWinner = QuarterOneWinner;
 		var SemiOneLoser = QuarterTwoWinner;
@@ -66,7 +66,7 @@ function updateBracket()
 		var SemiOneWinner = QuarterTwoWinner;
 		var SemiOneLoser = QuarterOneWinner;
 	}
-	
+
 	if (SemiTwoOneScore > SemiTwoTwoScore) {
 		var SemiTwoWinner = QuarterThreeWinner;
 		var SemiTwoLoser = QuarterFourWinner;
@@ -75,21 +75,21 @@ function updateBracket()
 		var SemiTwoWinner = QuarterFourWinner;
 		var SemiTwoLoser = QuarterThreeWinner;
 	}
-	
+
 	var FinalOneScore = algo(SemiOneWinner, SemiTwoWinner);
 	var FinalTwoScore = 5 - FinalOneScore;
-	
+
 	var LoserOneScore = algo(SemiOneLoser, SemiTwoLoser);
 	var LoserTwoScore = 5 - LoserOneScore;
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
 	var minData = {
 			teams : [
 				[OneOne,OneTwo],
@@ -102,15 +102,11 @@ function updateBracket()
 				[[SemiOneOneScore,SemiOneTwoScore], [SemiTwoOneScore,SemiTwoTwoScore]],
 				[[FinalOneScore,FinalTwoScore], [LoserOneScore,LoserTwoScore]
 				]
-			]	
+			]
 		}
 		$('.tournament').bracket({
 			init: minData
 		});
-	
-	
+
+
 }
-
-
-
-
